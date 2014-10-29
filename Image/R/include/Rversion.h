ed951added31a771301eecfbeb9b653d89cfb017
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-29 15:14:49 +0100

    Reconcile
    [ci skip]

--- Image/R/include/Rversion.h
+++ Image/R/include/Rversion.h
@@ -14,9 +14,9 @@ extern "C" {
 #define R_STATUS "Under development (unstable)"
 #define R_YEAR   "2014"
 #define R_MONTH  "11"
-#define R_DAY    "16"
-#define R_SVN_REVISION 66991
-#define R_FILEVERSION    3,20,66991,0
+#define R_DAY    "18"
+#define R_SVN_REVISION 66997
+#define R_FILEVERSION    3,20,66997,0
 
 #ifdef __cplusplus
 }
