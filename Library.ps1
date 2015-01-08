3a2a4970f56d53b0a6f0e92cb276abc59b40f4f5
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-08 22:32:05 +0100

    show download URLs as progress

--- Library.ps1
+++ Library.ps1
@@ -43,14 +43,16 @@ Function DownloadAndUnpack {
         Return
     }
 
-    Progress "Downloading R"
-    Invoke-WebRequest http://cran.r-project.org/bin/windows/base/R-devel-win.exe -OutFile .\DL\R-devel-win.exe
+    $rurl = "http://cran.r-project.org/bin/windows/base/R-devel-win.exe"
+
+    Progress "Downloading R from " + $rurl
+    Invoke-WebRequest $rurl -OutFile .\DL\R-devel-win.exe
 
     Progress "Determining Rtools version"
     $rtoolsver = $(Invoke-WebRequest http://cran.r-project.org/bin/windows/Rtools/VERSION.txt).Content.Split(' ')[2].Split('.')[0..1] -Join ''
     $rtoolsurl = "http://cran.r-project.org/bin/windows/Rtools/Rtools$rtoolsver.exe"
 
-    Progress "Downloading Rtools"
+    Progress "Downloading Rtools from " + $rtoolsurl
     Invoke-WebRequest $rtoolsurl -OutFile "DL\Rtools-current.exe"
 
     Progress "Preparing image"
